4b1a7888f2ee2062aedcd23be6cc0dea6c5c8f72
Kyriakos Kentzoglanakis <k.kentzoglanakis@pamediakopes.gr>
2019-02-08 15:28:00 +0200

    First take on the redrive function

--- redrive/main.go
+++ redrive/main.go
@@ -1,43 +1,73 @@
 package main
 
 import (
-	"bytes"
 	"context"
 	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strings"
+	"time"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/e-travel/goawsredrive/models"
 )
 
-// Response is of type APIGatewayProxyResponse since we're leveraging the
-// AWS Lambda Proxy Request functionality (default behavior)
-//
-// https://serverless.com/framework/docs/providers/aws/events/apigateway/#lambda-proxy-integration
-type Response events.APIGatewayProxyResponse
+var s3bucket = "TODO://"
 
-// Handler is our lambda handler invoked by the `lambda.Start` function call
-func Handler(ctx context.Context) (Response, error) {
-	var buf bytes.Buffer
+var client = &http.Client{
+	// TODO: Revise/parameterize timeout settings
+	Timeout: 10 * time.Second,
+}
 
-	body, err := json.Marshal(map[string]interface{}{
-		"message": "Go Serverless v1.0! Your function executed successfully!",
-	})
+var sess = session.Must(session.NewSession())
+var s3client = s3.New(sess)
+
+func Handler(ctx context.Context, event events.SQSEvent) error {
+	var err error
+	for _, msg := range event.Records {
+		err = ProcessMessage(msg)
+		if err != nil {
+			fmt.Printf(err.Error())
+			return err
+		}
+	}
+	return err
+}
+
+func ProcessMessage(message events.SQSMessage) error {
+	// load config from s3
+	input := &s3.GetObjectInput{
+		Bucket: aws.String(s3bucket),
+		Key:    aws.String(message.EventSourceARN),
+	}
+	result, err := s3client.GetObject(input)
 	if err != nil {
-		return Response{StatusCode: 404}, err
+		return err
 	}
-	json.HTMLEscape(&buf, body)
-
-	resp := Response{
-		StatusCode:      200,
-		IsBase64Encoded: false,
-		Body:            buf.String(),
-		Headers: map[string]string{
-			"Content-Type":           "application/json",
-			"X-MyCompany-Func-Reply": "hello-handler",
-		},
+	// deserialize config
+	config := models.StackConfiguration{}
+	body, err := ioutil.ReadAll(result.Body)
+	if err != nil {
+		return err
+	}
+	err = json.Unmarshal(body, &config)
+	if err != nil {
+		return err
 	}
 
-	return resp, nil
+	// good! now, send the message to the application
+	request, _ := http.NewRequest(config.HttpVerb, config.Endpoint, strings.NewReader(message.Body))
+	response, err := client.Do(request)
+	if response.StatusCode >= 300 {
+		return fmt.Errorf("%s %s code:%d error:%s",
+			config.HttpVerb, config.Endpoint, response.StatusCode, err.Error())
+	}
+	return nil
 }
 
 func main() {
